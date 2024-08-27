eeab469cde233b667fe905f8d791d7d21ed9ee94
Santhosh V S <138976032+Itssanthoshhere@users.noreply.github.com>
2024-08-27 11:00:30 +0530

    Add files via upload

---
+++ C++ with DSA-learning-journey/Day 1 - Diving into C++ Basics/Day1_IntroC++Basics.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> // Used to import all libraries
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Taking input
+    cout << "Input/Output: " << endl;
+    int x, y;
+    cin >> x >> y;
+    cout << "Value of x: " << x << endl;
+    cout << "Value of x: " << x << " and y: " << y;
+    
+    return 0;
+}
