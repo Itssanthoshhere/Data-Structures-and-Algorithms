4b457372a2e5793fd2f241f7e21058fc89f4b368
Santhosh V S <138976032+Itssanthoshhere@users.noreply.github.com>
2024-09-11 00:45:58 +0530

    Add files via upload

---
+++ C++ with DSA-learning-journey/Day30 - Pattern - 21 Hollow Rectangle Pattern/Pattern_21.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+void pattern21(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == 0 || j == 0 || i == n - 1 || j == n - 1) {
+                cout << "*";
+            }
+            else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    cout << "Pattern - 21: Hollow Rectangle Pattern" << endl;
+    int n;
+    cin >> n;
+    cout << "\n";
+    pattern21(n);
+}
+
+
+
