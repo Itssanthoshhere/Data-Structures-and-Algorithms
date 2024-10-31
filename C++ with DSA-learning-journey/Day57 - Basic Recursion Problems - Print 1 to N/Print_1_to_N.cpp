f7547fa4e29cbd62f8a9ef720dec0437f56a51ab
Santhosh V S <138976032+Itssanthoshhere@users.noreply.github.com>
2024-10-31 19:53:51 +0530

    Add files via upload

---
+++ C++ with DSA-learning-journey/Day57 - Basic Recursion Problems - Print 1 to N/Print_1_to_N.cpp
@@ -0,0 +1,62 @@
+                                                                                                                                                                                                                 /*                                                             
+2. Print 1 to N using Recursion
+
+-> Pseudo Code
+void f(i, n ) {
+   if (i > n) {
+   return; 
+   }
+   print(i);
+   f(i + 1, N);
+}
+
+int main() {
+  int n;
+  input(n);
+  f(1, n);
+}
+                                                                                                                                                                                                                                                                            */ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void func(int i, int n) {
+    // Base Condition.
+    if (i > n) return;
+
+    // Print the current value of i.
+    cout << i << endl;
+
+    // Recursive call to increment i until it reaches n.
+    func(i + 1, n);
+}
+
+int main() {
+    int n;
+
+    // Prompt user for input.
+    cout << "Enter the value of n: ";
+    cin >> n;
+
+    // Call the function with the initial value of i as 1.
+    func(1, n);
+
+    return 0;
+}
