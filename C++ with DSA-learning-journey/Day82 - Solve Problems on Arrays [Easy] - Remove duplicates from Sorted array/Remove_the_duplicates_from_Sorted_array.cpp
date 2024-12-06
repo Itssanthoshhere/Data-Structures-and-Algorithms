1e973bc7880a3d7e9a98acd3ec2e9788aa713c13
Santhosh V S <138976032+Itssanthoshhere@users.noreply.github.com>
2024-12-06 16:23:54 +0530

    Add files via upload

---
+++ C++ with DSA-learning-journey/Day82 - Solve Problems on Arrays [Easy] - Remove duplicates from Sorted array/Remove_the_duplicates_from_Sorted_array.cpp
@@ -0,0 +1,58 @@
+// Remove duplicates from Sorted array
+
+// Approach: Brute Force
+#include <bits/stdc++.h>
+using namespace std;
+
+int removeDuplicates(int arr[], int n) {
+    set <int> set;
+    for (int i = 0; i < n; i++) {
+        set.insert(arr[i]);
+    }
+
+    int k = set.size();
+    int j = 0;
+    for (int x : set) {
+        arr[j++] = x;
+    }
+
+    return k;
+}
+
+int main() {
+    int arr[] = {1,1,2,2,2,3,3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int k = removeDuplicates(arr, n);
+    cout << "The array after removing dulicates elements is" << endl;
+    for (int i = 0; i < k; i++) {
+        cout << arr[i] << " ";
+    }
+}
+
+
+
+// Optimal Approach (Two pointers)
+#include <bits/stdc++.h>
+using namespace std;
+
+int removeDuplicates(int arr[], int n) {
+    int i = 0;
+    for (int j = 1; j < n; j++) {
+        if (arr[i] != arr[j]) {
+            i++;
+            arr[i] = arr[j];
+        }
+    }
+    return i + 1;
+}
+
+int main() {
+    int arr[] = {1,1,2,2,2,3,3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int k = removeDuplicates(arr, n);
+
+    cout << "The array after removing duplictes elements is " << end;
+    for (int i = 0; i < k; i++) {
+        cout << arr[i] << " ";
+    }
+}
