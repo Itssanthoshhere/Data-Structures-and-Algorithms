164afa4e0ea362bba05f93dc15a974ec2beb1006
Santhosh V S <138976032+Itssanthoshhere@users.noreply.github.com>
2024-12-23 18:13:04 +0530

    Add files via upload

---
+++ C++ with DSA-learning-journey/Day90 - Solve Problems on Arrays [Easy] - Maximum Consecutive Ones/Maximum_Consecutive_Ones.cpp
@@ -0,0 +1,32 @@
+// Maximum Consecutive Ones
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution {
+    int findMaxConsecutiveOnes(vector<int> &nums) {
+        int cnt = 0;
+        int maxi = 0;
+        for (int i = 0; i < nums.size()l i++) {
+            if (nums[i] == 1) {
+                cnt++;
+            } else {
+                cnt = 0;
+            }
+
+            maxi = max(maxi, cnt);
+        }
+
+        return maxi;
+    }
+};
+
+int main() {
+    vector<int> nums = {1,1,0,1,1,1};
+    solution obj;
+
+    int ans = obj.findMaxConsecutiveOnes(nums);
+    cout << "The maximum consecutive 1's are " << ans;
+    
+    return 0;
+}
